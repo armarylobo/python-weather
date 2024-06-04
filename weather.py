b1cabcbab0eefe4a7c559bb4380563fb555b98a0
Armary Lobo <armary.lobo@gmail.com>
2024-06-04 14:35:20 +0800

    Added formatting

--- weather.py
+++ weather.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 def get_current_weather(city="Cebu City"):
 
-    request_url = f'\\
-        http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
-    
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={
+        os.getenv("API_KEY")}&q={city}&units=metric'
+
     weather_data = requests.get(request_url).json()
 
     return weather_data
